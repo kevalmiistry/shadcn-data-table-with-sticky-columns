dcc0a76fefcfb5c5d985d6a9751c45ddf3d0de78
kevalmiistry <kevalpm@gmail.com>
2024-12-20 01:24:31 +0530

    FEAT: adding page header w/ github repo. link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { DataTableWithStickyColumns } from "@/components/datatable-with-sticky-columns";
 import type { ApiResponse, User } from "@/types";
 import { columns } from "./columns";
+import { GithubIcon } from "lucide-react";
 
 export default async function Home() {
   const response = await fetch(
@@ -16,6 +17,21 @@ export default async function Home() {
 
   return (
     <section className="flex h-screen flex-col overflow-hidden">
+      <div className="flex items-center justify-between border-b p-4">
+        <h1 className="text-3xl font-semibold italic text-slate-600">
+          Shad-cn Data Table with Sticky Columns!
+        </h1>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/kevalmiistry/shadcn-data-table-with-sticky-columns"
+          className="rounded-full border-2 border-slate-600 p-1"
+        >
+          <GithubIcon className="text-slate-600" />
+        </a>
+      </div>
+
       {users ? (
         <DataTableWithStickyColumns
           columns={columns}
